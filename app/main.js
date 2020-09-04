0e628fef9697820a0431340dc39accec9e95581f
Nehemiah Boone <nehemiah.boone@gmail.com>
2020-09-04 13:09:28 -0600

    setup

--- app/main.js
+++ app/main.js
@@ -1,12 +1,17 @@
 import TodoController from "./controllers/TodoController.js";
 import WeatherController from "./controllers/WeatherController.js";
+import ImageController from "./Controllers/ImageController.js";
+import QuoteController from "./Controllers/QuoteController.js";
 
 
 //TODO Dont forget to register all your controllers	
 class App {
   constructor() {
-    this.weatherController = new WeatherController();
-    this.todoController = new TodoController();
+    this.weatherController = new WeatherController()
+    this.todoController = new TodoController()
+    this.imageController = new ImageController()
+    this.quoteController = new QuoteController()
+
   }
 }
 
