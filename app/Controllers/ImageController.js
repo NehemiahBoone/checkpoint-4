64caa3c6ac5a7f66381b871ca2366f7a8abd38ea
Nehemiah Boone <nehemiah.boone@gmail.com>
2020-09-04 13:02:33 -0600

    setup

--- app/Controllers/ImageController.js
+++ app/Controllers/ImageController.js
@@ -1,3 +1,5 @@
+import { ProxyState } from "../AppState.js"
+import imageService from "../Services/ImageService.js"
 
 //TODO Create methods for constructor, and rendering the image to the page
 //      (you may wish to set it as a background image: https://www.w3schools.com/JSREF/prop_style_backgroundimage.asp)
--- app/Controllers/QuoteController.js
+++ app/Controllers/QuoteController.js
@@ -1,3 +1,5 @@
+import { ProxyState } from "../AppState.js"
+import quoteService from "../Services/QuoteService.js"
 
 //TODO Create methods for constructor, and rendering the quote to the page
 export default class QuoteController { }
